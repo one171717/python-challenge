745862522af7d3427274c65ac86ef19d9b30bf2e
Christine Chynoweth <one171717@Christines-MacBook.local>
2018-05-31 15:53:04 -0400

    Fina final

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,39 +1,31 @@
 # import libraries
 import os
 	
-
 # navigate to file
-file = 'raw_data/paragraph_1.txt'
+file = 'raw_data/paragraph_2.txt'
 	
-
 # declare variables
 letter_count = 0
 	
-
 # open file
 with open(file, 'r') as txtfile:
 	paragraph = txtfile.read()
 	
-
-	# define word count
+    # define word count
 	word_count = paragraph.count(" ") + 1
 	
-
-	# define sentence count
+    # define sentence count
 	sentence_count = paragraph.count(".") + paragraph.count("!") + paragraph.count("?")
 	
-
     # calculate average letter count
 	for character in paragraph:
 	    if character.isalpha():
 	        letter_count += 1 
 	avg_letter_count = letter_count/word_count
 	
-
-	# define average sentence length
+    # define average sentence length
 	avg_sentence = word_count/sentence_count
 	
-
 # print analysis results
 print("Paragraph Analysis")
 print("------------------")
